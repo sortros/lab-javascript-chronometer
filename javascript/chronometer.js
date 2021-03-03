aeb313f2658ba1dbb542b348702e03abf437e213
sortros <albiol957@hotmail.com>
2021-03-03 17:05:38 +0100

    hecho iteracion 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,26 +1,65 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
-  startClick(callback) {
-    // ... your code goes here
+  startClick() {
+    
+    /*function increaseTime(() => {
+      this.currentTime ++;
+    })
+    const timeoutID = setInterval(increaseTime, 1000); */
+    this.intervalId = setInterval(()=>{
+      this.currentTime ++;
+
+    }, 1000)
   }
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
-    // ... your code goes here
+    
+    return (this.currentTime % 60) ;
+
   }
-  twoDigitsNumber() {
-    // ... your code goes here
+  twoDigitsNumber(num) {
+   /* let minString = parseFloat(this.getMinutes);
+    let secString = parseFloat(this.getSeconds);
+    
+    if (minString.length < 2) {
+      return `0 + ${minString}`;
+    }else{
+      return `0 + ${minString}`;
+    }; */
+
+    // if (secString.length < 2) {
+    //  return `0 + ${secString}`;
+    //}else{
+    //  return `${secString}` ;
+    //}  
+
+    if(num.toString().length < 2){
+      return "0" + num.toString();
+    } else {
+      return num.toString();
+    }
+
+   // if(minString.length === 2 && secString.length === 2){
+   //   return minString + secString;
+   // }
   }
   stopClick() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    
   }
   resetClick() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
   splitClick() {
-    // ... your code goes here
+    let finalMinutes = this.twoDigitsNumber(this.getMinutes());
+    let finalSeconds =this.twoDigitsNumber(this.getSeconds());
+
+    return `${finalMinutes}:${finalSeconds}`;
+
   }
 }
